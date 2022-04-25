98ef4657575a2db6e5b10fe8d01173f8e093c7ad
19520538 <19520538@gm.uit.edu.vn>
2022-04-25 20:39:46 +0700

    main

---
+++ XuLyData.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data = pd.read_csv("MeshliumDB_sensorData-210126.csv", header=None)
+df = data[[5,6,7]]
+df = df.pivot_table(index=7, columns=5, values=6, aggfunc='first')
+df.iloc[:9999999,:-1] = df.iloc[:9999999,:-1].fillna(0)
+df.to_csv('data.csv')
+print(df)
